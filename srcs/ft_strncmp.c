df935cbc2cf022afc5e527d907392391a29e1506
David Wuthrich <wut.dav@gmail.com>
2022-07-09 12:35:57 +0200

    bug fix

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dwuthric <dwuthric@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 14:31:36 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/07/07 14:57:53 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/07/09 12:27:31 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
+	if (n == 0)
+		return (0);
 	i = 0;
-	while (s1[i] == s2[i] && s1[i] && i < n - 1)
+	while (s1[i] && i < n - 1 && s1[i] == s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
