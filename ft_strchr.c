9cf34e1464ceff7b0e75e45377142953680240ae
David Wuthrich <dwuthric@student.42lausanne.ch>
2022-10-24 17:16:40 +0200

    edge case when c > 255

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: dwuthric <dwuthric@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 08:19:19 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/10/24 17:03:03 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/10/24 17:14:14 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ char	*ft_strchr(const char *s, int c)
 {
 	while (*s)
 	{
-		if (*s == c)
+		if (*s == c % 256)
 			return ((char *)s);
 		s++;
 	}
-	if (*s == c)
+	if (*s == c % 256)
 		return ((char *)s);
 	return (NULL);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: dwuthric <dwuthric@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 08:24:57 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/10/24 16:08:45 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/10/24 17:15:29 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_strrchr(const char *s, int c)
 		i++;
 	while (i >= 0)
 	{
-		if (s[i] == c)
+		if (s[i] == c % 256)
 			return ((char *)(s + i));
 		i--;
 	}
